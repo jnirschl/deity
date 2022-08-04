966fd43b022c049e1cd08800a046acb8d1584f5a
Jeff Nirschl <jnirsch@gmail.com>
2022-08-03 21:58:50 -0700

    Add skeleton to __main__.py. Add module imports to __init__.py. Update README.md

--- src/deity/__init__.py
+++ src/deity/__init__.py
@@ -1,1 +1,7 @@
 """DeITy."""
+__all__ = ["encode", "encode_filename", "main", "rename"]
+
+from .__encode__ import encode
+from .__encode__ import encode_filename
+from .__main__ import main
+from .__rename__ import rename
--- src/deity/__main__.py
+++ src/deity/__main__.py
@@ -1,12 +1,38 @@
-"""Command-line interface."""
+#!/usr/bin/env python3
+# """Command-line interface."""
+import logging
+from pathlib import Path
+
 import click
 
 
+# from dotenv import find_dotenv, load_dotenv
+
+
 @click.command()
+@click.argument(
+    "input_dir",
+    type=click.Path(exists=True),
+)
+@click.option("--ext", default="jpg,png", type=str, help="File extensions")
 @click.version_option()
-def main() -> None:
-    """DeITy."""
+def main(input_dir, ext):
+    """Click"""
+    pass
+    # file_list = []
+    # for suffix in ext.split(","):
+    #     file_list = Path(input_dir).glob()
 
 
 if __name__ == "__main__":
-    main(prog_name="deity")  # pragma: no cover
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    # load_dotenv(find_dotenv())
+
+    main()
