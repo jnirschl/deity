6e41f3650b2cc306e63d9091073dd250939000d6
Jeff Nirschl <jnirsch@gmail.com>
2022-08-03 22:01:39 -0700

    Preliminary testing skeleton for CLI to deity

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,41 @@
-"""Test cases for the __main__ module."""
 import pytest
 from click.testing import CliRunner
 
-from deity import __main__
+import deity
+from deity import main, encode
 
 
-@pytest.fixture
+@pytest.fixture()
 def runner() -> CliRunner:
     """Fixture for invoking command-line interfaces."""
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
-    assert result.exit_code == 0
+@pytest.fixture()
+def test_filename():
+    return "SHS-00-012345_part-A_cancer_40x_001.jpg"
+
+
+@pytest.fixture()  # scope="session"
+def tmp_file(tmpdir_factory, test_filename):
+    test_filename = "SHS-00-012345_part-A_cancer_40x_001.jpg"
+    fn = tmpdir_factory.mktemp("data").join(test_filename)
+    fn.write("")
+    return fn
+
+
+# @pytest.mark.skipif(
+#     deity.__version__ < "0.2.0", reason="not supported until version 0.2.0"
+# )
+@pytest.mark.smoke
+class TestMain:
+    def test_success(self, runner: CliRunner, tmp_file) -> None:
+        """It exits with a status code of zero."""
+        result = runner.invoke(main, ["./"])
+        assert tmp_file.exists()
+        assert result.exit_code == 0
+
+    def test_fail(self, runner: CliRunner) -> None:
+        """It exits with a status code of zero."""
+        result = runner.invoke(main, [""])
+        assert result.exit_code == 2
