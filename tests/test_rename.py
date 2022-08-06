50cea484377d98412433cca29904506311809d89
Jeff Nirschl <jnirsch@gmail.com>
2022-08-06 16:15:49 -0700

    Change dependabot.yml updates to weekly. Update poetry. Remove comment in test_rename.py

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -3,7 +3,7 @@ import pytest
 from deity import rename
 
 
-@pytest.fixture()  # scope="session"
+@pytest.fixture()
 def filepath(tmpdir_factory):
     test_filename = "SHS-00-012345_part-A_cancer_40x_001.jpg"
     fn = tmpdir_factory.mktemp("data").join(test_filename)
