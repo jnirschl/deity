285838213c6a27c3019eb0a89530583b6ad77439
Jeff Nirschl <jnirsch@gmail.com>
2022-08-07 13:42:34 -0700

    Use fixture test_files in test_rename.py.

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -1,20 +1,12 @@
 """Tests for src/deity/rename.py."""
-import pytest
+from pathlib import Path
 
 from deity import rename
 
 
-@pytest.fixture()
-def filepath(tmpdir_factory):
-    """Fixture for a temporary file."""
-    test_filename = "SHS-00-012345_part-A_cancer_40x_001.jpg"
-    fn = tmpdir_factory.mktemp("data").join(test_filename)
-    fn.write("")
-    return fn
-
-
-def test_rename(filepath):
+def test_rename(test_files):
     """Test that new filename is different from old filename."""
+    filepath = Path(test_files[0])
     new_filename = rename(filepath, dry_run=True)
     assert new_filename != filepath, AssertionError(
         f"New filename {filepath }is identical to input filename"
